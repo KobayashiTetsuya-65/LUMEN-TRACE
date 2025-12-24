5b13fd898dfc75ed7cb45cf414c1f13d4a8cde81
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2025-12-25 05:03:07 +0900

    [add]プレイヤーの画像入れ替え

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,19 @@ public class Player : MonoBehaviour
 {
     [Header("参照")]
     [SerializeField, Tooltip("見た目")] private SpriteRenderer _sr;
-    [SerializeField, Tooltip("通常時画像")] private Sprite[] _normalSprits;
+    [SerializeField, Tooltip("通常時画像")] private Sprite[] _normalSprites;
 
     [Header("数値設定")]
     [SerializeField, Tooltip("移動速度")] private float _speed = 2.0f;
+    [SerializeField, Tooltip("画像の入れ替え速度")] private float _spriteChangeDistance = 0.2f;
 
     private Rigidbody _rb;
     private PlayerInput _playerInput;
     private InputAction _moveAction;
 
     private Vector2 _moveInput;
+    private float _timer = 0;
+    private int _normalSpritesIndex = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -44,6 +47,17 @@ public class Player : MonoBehaviour
     }
     private void UpdateSprite()
     {
+        _timer += Time.deltaTime;
+        if (_timer >= _spriteChangeDistance)
+        {
+            _sr.sprite = _normalSprites[_normalSpritesIndex];
+            _normalSpritesIndex++;
+            _timer = 0;
+            if(_normalSpritesIndex >= _normalSprites.Length)
+            {
+                _normalSpritesIndex = 0;
+            }
+        }
         if (_moveInput.x > 0.01f)
             _sr.flipX = false;
         else if (_moveInput.x < -0.01f)

07fd5b9d3b89477a31ef6282c1bb810c1dfb82d2
KobayashiTetsuya <qtetsu.8201.tetsuq@gmail.com>
2026-01-17 15:07:58 +0900

    [add]ゴールポイント実装

---
+++ Assets/Scripts/ClearPoint.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ClearPoint : MonoBehaviour
+{
+    GameManager _gameManager;
+    private void Start()
+    {
+        _gameManager = GameManager.Instance;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            _gameManager.SceneMove("Title");
+        }
+    }
+}
